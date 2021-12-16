3bd1a0a3397f22bba83cb75b766f6de90ac5decb
Shaun Karran <shaun.karran@polymathian.com>
2021-12-16 16:26:21 +1000

    Day 6, part 2. Thanks John.

--- src/day6.rs
+++ src/day6.rs
@@ -1,44 +1,72 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let fish = fs::read_to_string("input/day6")
         .expect("Something went wrong reading the file.")
         .split(',')
-        .map(|a| a.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+        .map(|a| a.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
 
     dbg!(part1(&fish));
     dbg!(part2(&fish));
 }
 
-fn part1(fish: &Vec<i32>) -> usize {
-    let mut part1_fish = fish.clone();
-
+fn part1(fishies: &Vec<i64>) -> i64 {
+    let mut counts = HashMap::from([
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (4, 0),
+        (5, 0),
+        (6, 0),
+        (7, 0),
+        (8, 0),
+    ]);
+    for &fish in fishies {
+        counts.entry(fish).and_modify(|count| *count += 1).or_insert(1);
+    }
     for _day in 0..80 {
-        part1_fish = update(&part1_fish);
+        counts = new_update(counts);
     }
 
-    part1_fish.len()
+    counts.into_values().sum()
 }
 
-fn part2(fish: &Vec<i32>) -> usize {
-    0
+fn part2(fishies: &Vec<i64>) -> i64 {
+    let mut counts = HashMap::from([
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (4, 0),
+        (5, 0),
+        (6, 0),
+        (7, 0),
+        (8, 0),
+    ]);
+    for &fish in fishies {
+        counts.entry(fish).and_modify(|count| *count += 1).or_insert(1);
+    }
+    for _day in 0..256 {
+        counts = new_update(counts);
+    }
+
+    counts.into_values().sum()
 }
 
-fn update(fish: &Vec<i32>) -> Vec<i32> {
-    let mut updated_fish = Vec::new();
-    let mut fish_to_add = 0;
-    for fish in fish {
-        if *fish == 0 {
-            updated_fish.push(6);
-            fish_to_add += 1;
-        }
-        else {
-            updated_fish.push(fish - 1);
-        }
-    }
-    for _ in 0..fish_to_add {
-        updated_fish.push(8);
-    }
-    updated_fish
+fn new_update(counts: HashMap<i64, i64>) -> HashMap<i64, i64> {
+    let mut new_counts: HashMap<i64, i64> = HashMap::new();
+    let number_of_6s = counts.get(&0).unwrap() + counts.get(&7).unwrap();
+    new_counts.insert(0, *counts.get(&1).unwrap());
+    new_counts.insert(1, *counts.get(&2).unwrap());
+    new_counts.insert(2, *counts.get(&3).unwrap());
+    new_counts.insert(3, *counts.get(&4).unwrap());
+    new_counts.insert(4, *counts.get(&5).unwrap());
+    new_counts.insert(5, *counts.get(&6).unwrap());
+    new_counts.insert(6, number_of_6s);
+    new_counts.insert(7, *counts.get(&8).unwrap());
+    new_counts.insert(8, *counts.get(&0).unwrap());
+
+    new_counts
 }
